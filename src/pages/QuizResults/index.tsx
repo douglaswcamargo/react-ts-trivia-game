32dab5987ab8f8c78dc7789076554dca7139b0f0
douglas <d.waligura@cinq.com.br>
2020-09-02 18:56:43 -0300

    Add results page

--- src/pages/QuizResults/index.tsx
+++ src/pages/QuizResults/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import he from 'he'
+import { useTranslation } from 'react-i18next'
+import QuizContainer from '../../containers/QuizContainer'
+import * as S from './style'
+import LinkButton from '../../components/LinkButton'
+import Title from '../../components/Title'
+import Text from '../../components/Text'
+import { useSelector } from 'react-redux'
+import { AppState } from '../../store/types'
 
-export default function Results () {
+export default function QuizResults () {
+  const { t } = useTranslation()
+  const answers = useSelector((state: AppState) => state.answers.data)
+  const correctAnswers = answers.filter((answer) => !answer.correct)
+
   return (
-    <div>
-      Results
-    </div>
+    <QuizContainer>
+      <S.Wrapper>
+        <Title>{t('quiz.resultsTitle')}</Title>
+        <Text as="h1">{correctAnswers.length} / {answers.length}</Text>
+        <S.List>
+          {answers.map((answer, key) => {
+            return (
+              <S.ListItem key={key} correct={answer.correct}>
+                {answer.correct ? ' + ' : ' - '}
+                {he.decode(answer.question)}
+              </S.ListItem>
+            )
+          })}
+        </S.List>
+      </S.Wrapper>
+      <LinkButton to="/quiz">{t('quiz.resultsButtonRetry')}</LinkButton>
+    </QuizContainer>
   )
 }
--- src/pages/QuizResults/style.tsx
+++ src/pages/QuizResults/style.tsx
@@ -6,8 +6,23 @@ export const TextWithPadding = styled.p`
     padding-bottom: 38px;
 `
 export const Wrapper = styled.div`
-    height: 446px;
+    min-height: 446px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+`
+export const List = styled.ul`
+    max-height: 320px;
+    overflow: auto;
+    list-style: none;
+    margin-left: 0;
+    padding-left: 0;
+    margin-bottom: 25px;
+`
+export const ListItem = styled.li<{correct: boolean}>`
+    padding-left: 1em;
+    text-indent: -0.8em;
+    margin-bottom: 1rem;
+    text-align: left;
+    color: ${(props) => props.correct ? props.theme.colors.success : props.theme.colors.error};
 `
