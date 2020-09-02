abf58327245b432c50fac81b6e2796138c68194b
douglas <d.waligura@cinq.com.br>
2020-09-02 18:42:13 -0300

    Add themes

--- src/config/themes.ts
+++ src/config/themes.ts
@@ -1,14 +1,33 @@
-export default {
+const light = {
   colors: {
-    primary: 'rgb(68, 169, 255)',
+    primary: '#44a9ff',
     success: '#2ac498',
     error: '#f33236',
-    text: 'rgb(45, 57, 86)',
+    text: '#2d3956',
     control: '#C0D0E6',
     icon: '#7F8B97',
-    background: 'rgb(250, 250, 250)'
+    bodyBackground: '#fafafa',
+    sectionBackground: '#e0e0e0'
   },
   fonts: {
-    primary: `'Helvetica Now', Helvetica, Arial, sans-serif`
+    primary: '\'Helvetica Now\', Helvetica, Arial, sans-serif'
   }
 }
+
+const dark = {
+  colors: {
+    primary: 'rgba(68, 169, 255, .7)',
+    success: '#2ac498',
+    error: '#f33236',
+    text: '#fafafa',
+    control: '#C0D0E6',
+    icon: '#7F8B97',
+    bodyBackground: '#363537',
+    sectionBackground: '#1c1b1b'
+  },
+  fonts: {
+    primary: '\'Helvetica Now\', Helvetica, Arial, sans-serif'
+  }
+}
+
+export { light, dark }
