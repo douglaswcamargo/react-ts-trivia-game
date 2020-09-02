9fc0404a0c9281cc276bd8af735784cc6aa8d24b
douglas <d.waligura@cinq.com.br>
2020-09-02 18:53:23 -0300

    create 404 error page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,21 +1,9 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import QuizContainer from '../../containers/QuizContainer'
-import * as S from './style'
-import LinkButton from '../../components/LinkButton'
 import Title from '../../components/Title'
 
 export default function QuizHome () {
   const { t } = useTranslation()
 
-  return (
-    <QuizContainer>
-      <S.Wrapper>
-        <Title>{t('quiz.homeTitle')}</Title>
-        <S.TextWithPadding>{t('quiz.homeDescription')}</S.TextWithPadding>
-        <S.TextWithPadding>{t('quiz.homeChallenge')}</S.TextWithPadding>
-      </S.Wrapper>
-      <LinkButton to="/quiz">{t('quiz.homeButtonStart')}</LinkButton>
-    </QuizContainer>
-  )
+  return <Title>{t('general.notFound')}</Title>
 }
