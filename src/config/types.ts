9226159d18e6a875aa8e12a8812af051327ddb7b
douglas <d.waligura@cinq.com.br>
2020-09-02 18:43:58 -0300

    Add global style types

---
+++ src/config/types.ts
@@ -0,0 +1,15 @@
+export interface CustomTheme {
+  colors: {
+    primary: string
+    success: string
+    error: string
+    text: string
+    control: string
+    icon: string
+    bodyBackground: string
+    sectionBackground: string
+  }
+  fonts: {
+    primary: string
+  }
+}
