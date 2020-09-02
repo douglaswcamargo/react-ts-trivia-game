8fceac1726b7c0d483ce9ab1e8bad8f54d3784eb
douglas <d.waligura@cinq.com.br>
2020-09-02 18:48:12 -0300

    create index & app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import Text from './Text'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import store from './store'
+import './i18n/'
+import Routes from './routes/routes'
+import AppThemeProvider from './contexts/AppThemeProvider'
 
 function App () {
   return (
-    <Text>hello</Text>
+    <Provider store={store}>
+      <BrowserRouter>
+        <AppThemeProvider>
+          <Routes />
+        </AppThemeProvider>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
