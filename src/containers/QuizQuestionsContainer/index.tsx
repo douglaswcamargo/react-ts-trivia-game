49db9852c565f65d4c931896d046e2ee5be98c9f
douglas <d.waligura@cinq.com.br>
2020-09-02 18:56:20 -0300

    Create question answer container

---
+++ src/containers/QuizQuestionsContainer/index.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import { useHistory } from 'react-router-dom'
+import { fetchQuestions } from '../../store/ducks/questions/actions'
+import { answerQuestion } from '../../store/ducks/answers/actions'
+import { AppState } from '../../store/types'
+import Button from '../../components/Button'
+import ButtonGroup from '../../components/ButtonGroup'
+import QuizQuestion from '../../components/QuizQuestion'
+import Spinner from '../../components/Spinner'
+import { Answer } from '../../store/ducks/answers/types'
+import Title from '../../components/Title'
+
+export default function QuizQuestionContainer () {
+  const questions = useSelector((state: AppState) => state.questions)
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const { t } = useTranslation()
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const handleAnswer = (answer: string) => {
+    const answerObj: Answer = {
+      question: questions.data[currentQuestion].question,
+      correct: answer === questions.data[currentQuestion].correct_answer
+    }
+
+    dispatch(answerQuestion(answerObj))
+    if (currentQuestion + 1 < questions.data.length) {
+      setCurrentQuestion(currentQuestion + 1)
+    } else {
+      history.push('/results')
+    }
+  }
+
+  useEffect(() => {
+    dispatch(fetchQuestions())
+  }, [])
+
+  if (questions.loading) {
+    return <Spinner />
+  }
+
+  return (
+    <>
+      <Title as="h2">{questions.data[currentQuestion].category}</Title>
+      <QuizQuestion
+        totalQuestions={questions.data.length}
+        currentQuestion={currentQuestion + 1}
+        question={questions.data[currentQuestion].question}
+      />
+      <ButtonGroup>
+        <Button title={t('quiz.quizButtonFalse')} onClick={() => handleAnswer('True')} />
+        <Button title={t('quiz.quizButtonTrue')} onClick={() => handleAnswer('False')} />
+      </ButtonGroup>
+    </>
+  )
+}
