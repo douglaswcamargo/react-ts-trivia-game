986918f3c79a9656419dee1f98cad4f9422f8ceb
douglas <d.waligura@cinq.com.br>
2020-09-12 20:41:53 -0300

    Add new translated fields

--- src/i18n/en_US.ts
+++ src/i18n/en_US.ts
@@ -4,14 +4,18 @@ export default {
     notFound: 'Page not found.'
   },
   quiz: {
-    homeTitle: 'Welcome to the Trivia Challenge',
+    homeTitle: 'Trivia Challenge',
+    homeSubTitle: 'Welcome!',
     homeDescription: 'You will be presented with 10 True or False questions',
     homeChallenge: 'Can you score 100%?',
-    homeButtonStart: 'Begin',
+    homeButtonStart: 'Press (A) or (B) to start!',
     quizButtonTrue: 'True',
     quizButtonFalse: 'False',
+    quizHintA: '(A) True',
+    quizHintB: '(B) False',
     quizCounterPreposition: 'of',
     resultsTitle: 'You scored',
-    resultsButtonRetry: 'Play Again'
+    resultsButtonRetry: 'Play Again',
+    resultsHint: 'Press (A) or (B) to try again'
   }
 }
--- src/i18n/pt_BR.ts
+++ src/i18n/pt_BR.ts
@@ -4,14 +4,18 @@ export default {
     notFound: 'Página não encontrada.'
   },
   quiz: {
-    homeTitle: 'Bem-vindo ao Desafio de Trivia',
-    homeDescription: 'Serão apresentadas 10 questões que você deve responder com Verdadeiro ou Falso',
+    homeTitle: 'Desafio de Trivia',
+    homeSubTitle: 'Bem-vindo(a)!',
+    homeDescription: 'São 10 questões que você deve responder com Verdadeiro ou Falso',
     homeChallenge: 'Você consegue acertar 100%?',
-    homeButtonStart: 'Iniciar',
+    homeButtonStart: 'Pressione (A) ou (B) para iniciar!',
     quizButtonTrue: 'Verdadeiro',
     quizButtonFalse: 'Falso',
+    quizHintA: '(A) Verdadeiro',
+    quizHintB: '(B) Falso',
     quizCounterPreposition: 'de',
     resultsTitle: 'Resultado',
-    resultsButtonRetry: 'Jogar Novamente'
+    resultsButtonRetry: 'Jogar Novamente',
+    resultsHint: 'Pressione (A) ou (B) para jogar novamente'
   }
 }
