79da89bba1bc1ce2da01dd29df1b7242cea93eda
douglas <d.waligura@cinq.com.br>
2020-09-12 11:55:20 -0300

    Updates

---
+++ precache-manifest.4754c01272927ce987d9556b46debd05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225f611c72b6836ea93ef85b677c532b",
+    "url": "/react-ts-trivia-game/index.html"
+  },
+  {
+    "revision": "3a8683f32378b67757c0",
+    "url": "/react-ts-trivia-game/static/js/2.4dca03d0.chunk.js"
+  },
+  {
+    "revision": "12ac8af0741afe107c3dcb4ef10e1ce5",
+    "url": "/react-ts-trivia-game/static/js/2.4dca03d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcde901107528acdf01a",
+    "url": "/react-ts-trivia-game/static/js/main.e3e86687.chunk.js"
+  },
+  {
+    "revision": "5c1101fbc7d58801d699",
+    "url": "/react-ts-trivia-game/static/js/runtime-main.48e1660b.js"
+  }
+]);
