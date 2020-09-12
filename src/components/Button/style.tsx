bd9e3de6a420f405bb48f0676a69a4c516ab39f1
douglas <d.waligura@cinq.com.br>
2020-09-12 20:42:20 -0300

    Refactor button

--- src/components/Button/style.tsx
+++ src/components/Button/style.tsx
@@ -1,21 +1,31 @@
 import styled from 'styled-components'
 
 export const Container = styled.button`
+width: 48px;
+height: 48px;
+  text-decoration: none;
   display: block;
   padding: 15px;
-  font-family: ${(props) => props.theme.fonts.primary};
-  font-size: 1.1rem;
+  font-family: ${(props) => props.theme.fonts.handheldtext};
+  font-size: 1rem;
   font-weight: bold;
   text-transform: uppercase;
-  color: ${(props) => props.theme.colors.primary};
-  background-color: transparent;
-  border: 2pt solid ${(props) => props.theme.colors.primary};
-  border-radius: 4px;
+  color: #fff;
+  background-color: ${(props) => props.theme.colors.handheldSecondary};
+  border: none;
+  border-radius: 100%;
   cursor: pointer;
-  min-width: 153px;
+  box-shadow: 0px 4px 0px ${(props) => props.theme.colors.handheldSecondaryShadow};
+
   &[disabled] {
     cursor: not-allowed;
-    opacity: 0.7;
+    opacity: 0.8;
     color: #d1d1d1;
   }
+  
+  &:active{
+    box-shadow: 0px 2px 0px ${(props) => props.theme.colors.handheldSecondaryShadow};
+    position: relative;
+    top: 2px;
+}
 `
